4d2a9c3ea07899240eb002031170f54dffce9ca1
Mazen Ramadan <mazenramadan00@gmail.com>
2023-03-13 11:25:33 +0200

    Add create order service

--- api-gateway/src/app.service.ts
+++ api-gateway/src/app.service.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices/client';
+import { CreateOrderRequest } from './create-order-request.dto';
+import { OrderCreatedEvent } from './order-created.event';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @Inject ('BILLING_SERVICE') private readonly billingClient: ClientKafka,
+  ) {}
+  
+  createOrder({ userId, price }: CreateOrderRequest) {
+    this.billingClient.emit(
+      'order_created',
+      new OrderCreatedEvent('123', userId, price)
+    )
   }
 }
