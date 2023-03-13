812bea876b4fc39ed795313a8c6803ce35cdfcbc
Mazen Ramadan <mazenramadan00@gmail.com>
2023-03-13 11:24:16 +0200

    Add kafka broker

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices/enums';
+import { ClientsModule } from '@nestjs/microservices/module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'BILLING_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'billing',
+            brokers: ['127.0.0.1:9092']
+          },
+          consumer: {
+            groupId: 'billing-consumer'
+          }
+        }
+      }
+    ])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
