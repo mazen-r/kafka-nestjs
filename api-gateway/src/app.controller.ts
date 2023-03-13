9d8e46f61ddcd1ddddc3b8f4c68a341b27bbbfa0
Mazen Ramadan <mazenramadan00@gmail.com>
2023-03-13 11:24:54 +0200

    Add create order controller

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateOrderRequest } from './create-order-request.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  createOrder(@Body() createOrderRequest: CreateOrderRequest) {
+    this.appService.createOrder(createOrderRequest);
   }
 }
