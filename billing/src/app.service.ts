c7c97275db9cf334efdd7ee139cfbf7984c25c04
Mazen Ramadan <mazenramadan00@gmail.com>
2023-03-22 00:04:13 +0200

    Add billing service

--- billing/src/app.service.ts
+++ billing/src/app.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+import { GetUserRequest } from './get-user-request.dto';
+import { OrderCreatedEvent } from './order-created.event';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+  constructor(
+    @Inject('AUTH_SERVICE') private readonly authClient: ClientKafka) {}
+  
+    handleOrderCreated(orderCreatedEvent: OrderCreatedEvent) {
+      this.authClient
+        .send('get_user', new GetUserRequest(orderCreatedEvent.userId))
+        .subscribe((user) => {
+          console.log(
+            `Billing user with stripe ID ${user.stripeUserId} a price of $${orderCreatedEvent.price}...`,
+          );
+        });
+    }  
 }
